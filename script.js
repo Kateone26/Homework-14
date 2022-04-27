bf41e9b7e0fc6deaddaa158d129b055d7ab8db89
Kateone26 <101978392+Kateone26@users.noreply.github.com>
2022-04-27 20:54:21 +0400

    script.js

---
+++ script.js
@@ -0,0 +1,116 @@
+// 1.
+
+// let array = [5,7,3,13,9,6];
+
+// array.push('javascript', 'python');
+// console.log(array);
+
+// array.unshift('html','css');
+// console.log(array);
+
+// console.log(array.length);
+
+// array.shift();
+// console.log(array);
+
+// array.pop();
+// console.log(array);
+
+// array.splice(3,0,150);
+// console.log(array);
+
+
+
+// 2.
+
+// let array = ['orange','banana','pear'];
+
+// console.log(array.length);
+
+// array.push('apple', 'pineapple');
+
+// array.unshift('watermelon');
+
+// console.log(array.length);
+
+// array.splice(2,0,'mango');
+
+// array.shift();
+
+// array.pop();
+
+// console.log(array);
+
+// console.log(array.length);
+
+
+// 3.
+
+// let numbers = [12, 25, 3, 6, 8, 14, 7, 23];
+// let result = numbers.map(function(x){
+//     return x / 3;
+// })
+// console.log(result);
+
+// 4.
+
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let newarray = array.filter(x => typeof x == 'number');
+// console.log(newarray);
+
+// 5.
+
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+// let newarray = languages.filter(x => x.length>3);
+// console.log(newarray);
+
+// 6.
+
+// let array= ['academy', 'of', 'digital', 'industries'].reduce(function(x,y){
+//     return x.concat(y+' ');
+//     },
+//     ''
+// )
+// console.log(array);
+
+
+
+
+
+
+
+
+// 7. ????????????????????
+
+// 7. მოცემულია მასივი:
+// let item = [12, ‘google’, 32, null, ‘yahoo’, 25];
+
+// შექმენით ახალი მასივი map ფუნქციის გამოყენებით შემდეგი ლოგიკით: 
+// თუ ელემენტი არის რიცხვი ამ მასივში უნდა იყოს ამ რიცხვის კვადრატი. 
+// თუ ელემენტი არის ტექსტი, მაშინ ახალ მასივში უნდა იყოს ეს ტექსტი დიდი ასოებით.
+//  სხვა შემთხვევაში ელემენტები უნდა დაემატოს უცვლელად;
+
+
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 8.
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+
+// let newarray = words.filter(x => x.includes('m') || x.includes('M'));
+
+// console.log(newarray);
